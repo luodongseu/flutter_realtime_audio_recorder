710ae445a0ad762ef23532760ca7d05be5d5d8a2
moresales <luodong@moresales.cn>
2019-12-09 10:51:58 +0800

    update

---
+++ ios/Classes/DataHandler.h
@@ -0,0 +1,16 @@
+//
+//  DataHandler.h
+//  realtime_audio_recorder
+//
+//  Created by luodong on 2019/12/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DataHandler : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/Classes/DataHandler.m
@@ -0,0 +1,12 @@
+//
+//  DataHandler.m
+//  realtime_audio_recorder
+//
+//  Created by luodong on 2019/12/9.
+//
+
+#import "DataHandler.h"
+
+@implementation DataHandler
+
+@end
