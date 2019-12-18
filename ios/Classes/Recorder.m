39bfcb7aec34c3baba47c67bf82bbede225766de
luodong <luodong@moresales.cn>
2019-12-18 10:42:29 +0800

    update

--- ios/Classes/Recorder.m
+++ ios/Classes/Recorder.m
@@ -64,7 +64,7 @@ lame_t lame;
     lame = lame_init();
     lame_set_num_channels(lame, 1);//通道
     lame_set_in_samplerate(lame, [inputNode inputFormatForBus:0].sampleRate / 2);//采样率
-    lame_set_brate(lame, 96);//比特率
+    // lame_set_brate(lame, 96);//比特率
     lame_set_quality(lame, 2);//音质
     lame_set_out_samplerate(lame, 0);
     //    lame_set_mode(lame, 3);
